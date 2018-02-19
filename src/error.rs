67673f771526df247dc36c39d8f4c2bf3137d630
Chris McDonald <mail.chris.mcdonald@gmail.com>
2018-02-19 11:06:22 -0700

    remove spurious error variant match arm

--- src/error.rs
+++ src/error.rs
@@ -12,7 +12,6 @@ impl Display for DedupError {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
         match *self {
             DedupError::ClosedPipe => write!(f, "A closed pipe was encountered"),
-            DedupError::Other => write!(f, "An unknown error has occurred"),
             DedupError::IO(ref i) => write!(f, "{}", i),
             DedupError::ArgumentParseError(ref s) => write!(f, "{}", s),
         }
