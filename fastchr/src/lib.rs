46e2d7ec376a166c3532a37dfc0bb4f1d798636f
Chris McDonald <mail.chris.mcdonald@gmail.com>
2018-02-27 14:13:09 -0700

    added tests and fixed broken comparison

--- fastchr/src/lib.rs
+++ fastchr/src/lib.rs
@@ -28,16 +28,15 @@ pub fn fastchr(needle: u8, haystack: &[u8]) -> Option<usize> {
 }
 
 #[inline]
-fn v_to_byte_mask(mut v: u8s, needle: u8) -> usize {
+fn v_to_byte_mask(v: u8s, needle: u8) -> usize {
     unsafe {
-        v ^= u8s(needle);
-        v -= u8s(1);
+        let v = v.eq(u8s(needle));
 
         #[cfg(all(not(target_feature = "avx"), target_feature = "sse2"))]
-        { _mm_movemask_epi8(v.into()) as usize }
+        { _mm_movemask_epi8(v) as usize }
         
         #[cfg(target_feature = "avx")]
-        { _mm256_movemask_epi8(v.into()) as usize }
+        { _mm256_movemask_epi8(v) as usize }
     }
 }
 
@@ -58,80 +57,107 @@ mod tests {
     const LONG_PREFIX_LEN: usize = 500000;
     const SHORT_PREFIX_LEN: usize = u8s::WIDTH * 2 - 1;
     const ODD_PREFIX_LEN: usize = 50003;
+    const BURIED_SAMPLE_LEN: usize = 241;
+    const NEEDLE: u8 = 70;
 
     fn generate_long_sample() -> Vec<u8> {
             iter::repeat(14u8).take(LONG_PREFIX_LEN)
-            .chain(iter::once(70u8))
+            .chain(iter::once(NEEDLE))
             .chain(iter::repeat(200u8).take(ODD_PREFIX_LEN))
             .collect()
     }
 
     fn generate_short_sample() -> Vec<u8> {
             iter::repeat(14u8).take(SHORT_PREFIX_LEN)
-            .chain(iter::once(70u8))
+            .chain(iter::once(NEEDLE))
             .collect()
     }
 
     fn generate_odd_sample() -> Vec<u8> {
             iter::repeat(14u8).take(ODD_PREFIX_LEN)
-            .chain(iter::once(70u8))
+            .chain(iter::once(NEEDLE))
             .collect()
+    }
+    fn generate_long_negative_sample() -> Vec<u8> {
+        iter::repeat(231).take(ODD_PREFIX_LEN)
+        .collect()
+    }
+
+    fn generate_buried_sample() -> Vec<u8> {
+        iter::repeat(231u8).take(BURIED_SAMPLE_LEN)
+        .chain(iter::once(NEEDLE))
+        .chain(iter::repeat(231u8).take(3))
+        .chain(iter::once(NEEDLE))
+        .chain(iter::repeat(231u8).take(ODD_PREFIX_LEN))
+        .collect()
+    }
+
+    #[test]
+    fn buried_sample_test() {
+        let data = generate_buried_sample();
+        assert_eq!(Some(BURIED_SAMPLE_LEN), fastchr(NEEDLE, &data));
+    }
+
+    #[test]
+    fn negative_sample_test() {
+        let data = generate_long_negative_sample();
+        assert_eq!(None, fastchr(NEEDLE, &data));
     }
     
     #[test]
-    fn example_test() {
+    fn qbf_test() {
         let haystack = b"the quick brown fox";
         println!("{:?}", haystack);
         println!("{:?}", (&haystack[..]).simd_iter().next_vector());
-        assert_eq!(fastchr(b'k', haystack), Some(8));
+        assert_eq!(Some(8), fastchr(b'k', haystack));
     }
 
     #[test]
     fn memchr_odd_compat_test() {
         let data = generate_odd_sample();
-        assert_eq!(memchr(70, &data), fastchr(70, &data));
+        assert_eq!(memchr(NEEDLE, &data), fastchr(NEEDLE, &data));
     }
 
     #[test]
     fn memchr_short_compat_test() {
         let data = generate_short_sample();
-        assert_eq!(memchr(70, &data), fastchr(70, &data));
+        assert_eq!(memchr(NEEDLE, &data), fastchr(NEEDLE, &data));
     }
 
     #[test]
     fn long_find_test() {
         let data = generate_long_sample();
-        assert_eq!(Some(LONG_PREFIX_LEN), fastchr(70u8, &data));
+        assert_eq!(Some(LONG_PREFIX_LEN), fastchr(NEEDLE, &data));
     }
 
     #[test]
     fn short_find_test() {
         let data = generate_short_sample();
-        assert_eq!(Some(SHORT_PREFIX_LEN), fastchr(70u8, &data));
+        assert_eq!(Some(SHORT_PREFIX_LEN), fastchr(NEEDLE, &data));
     }
 
     #[test]
     fn odd_find_test() {
         let data = generate_odd_sample();
-        assert_eq!(Some(ODD_PREFIX_LEN), fastchr(70u8, &data));
+        assert_eq!(Some(ODD_PREFIX_LEN), fastchr(NEEDLE, &data));
     }
 
     #[test]
     fn empty_find_test() {
         let data = Vec::new();
-        assert_eq!(None, fastchr(70u8, &data));
+        assert_eq!(None, fastchr(NEEDLE, &data));
     }
 
     #[bench]
     fn fastchr_bench(b: &mut Bencher) {
         let data = generate_long_sample();
-        b.iter(|| fastchr(70u8, &data));
+        b.iter(|| fastchr(NEEDLE, &data));
     }
 
     #[bench]
     fn memchr_bench(b: &mut Bencher) {
         let data = generate_long_sample();
-        b.iter(|| memchr(70u8, &data));
+        b.iter(|| memchr(NEEDLE, &data));
     }
 
 }
